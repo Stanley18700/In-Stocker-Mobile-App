73304af17bb9869db43b7b42ca700e068e4a5512
Stan1080 <6731201085@lamduan.mfu.ac.th>
2026-04-14 23:36:56 +0700

    Bundle Ionicons font as app asset

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
 import AppNavigator from './src/core/navigation/AppNavigator';
 
 export default function App() {
-  const [fontsLoaded] = useFonts(Ionicons.font);
+  const [fontsLoaded] = useFonts({
+    Ionicons: require('./assets/fonts/Ionicons-v2.ttf'),
+  });
 
   if (!fontsLoaded) {
     return null;
