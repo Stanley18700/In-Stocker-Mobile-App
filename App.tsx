080b77a33c27a53ccfcef791e4d2a04951add851
Stan1080 <6731201085@lamduan.mfu.ac.th>
2026-04-14 23:06:41 +0700

    Fix icon fonts on web

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import AppNavigator from './src/core/navigation/AppNavigator';
 
 export default function App() {
+  const [fontsLoaded] = useFonts(Ionicons.font);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar style="dark" />
